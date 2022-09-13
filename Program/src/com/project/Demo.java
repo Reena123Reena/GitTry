25bc52fba9f39a3b5b0ce043d7888374e83a52eb
Reena <attardereena@gmail.com>
2022-09-13 22:50:55 +0530

    second code

---
+++ Program/src/com/project/Demo.java
@@ -0,0 +1,12 @@
+package com.project;
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo emp = new Demo();
+		Demo emp1 = new Demo();
+		boolean ans = emp.equals(emp1);
+		System.out.println(ans);
+	}
+
+}
